49c3d0502d07c21a92c281968f9daddf44dd9ca7
Eddie Woo <archaco123@gmail.com>
2024-11-22 16:29:05 +0900

    부록 - 데이터 추가 SQL

---
+++ schema.sql
@@ -0,0 +1,32 @@
+drop database if exists `est-hibernate-test`;
+create database `est-hibernate-test`;
+use `est-hibernate-test`;
+
+-- team 추가
+insert into `teams` ( name ) VALUES ('한화_이글스' );
+insert into `teams` ( name ) VALUES ('키움_히어로즈' );
+insert into `teams` ( name ) VALUES ('롯데_자이언츠' );
+
+-- player 추가
+insert into `players` ( name, team_id )
+values ( '위니', 1 );
+
+insert into `players` ( name, team_id )
+values ( '비니', 1 );
+
+insert into `players` ( name, team_id )
+values ( '수리',1 );
+
+insert into `players` ( name, team_id )
+values ( '턱돌이',  2 );
+
+insert into `players` ( name, team_id )
+values ( '동글이',  2 );
+
+insert into `players` ( name, team_id )
+values ( '누리',  3 );
+
+insert into `players` ( name, team_id )
+values ( '아라',  3 );
+
+
